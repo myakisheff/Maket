934484c543029f1e3df2e132958a7271d369dc61
myakisheff <n.s.myakishev@yandex.ru>
2023-03-25 11:27:43 +0300

    added form invalid messages

--- scripts/registration.js
+++ scripts/registration.js
@@ -1,29 +1,77 @@
 const button = document.querySelector('button');
-const form = document.querySelector('#reg');
+const divform = document.querySelector('#reg');
 const popup = document.querySelector('.popup');
 const passView = document.querySelector('.password-control');
 const passInput = document.querySelector('#password-input');
+const form = document.querySelector("#regform");
 
+const emailInput = document.querySelector('[name="Email"]'); 
+
+//show a modal window
 button.addEventListener('click', () => {
   popup.style.display = "block";
-  form.style.display = "block";
+  divform.style.display = "block";
 });
 
 popup.addEventListener('click', function(e) {
   if (e.target.id == 'popup1') {
     popup.style.display = "none";
-    form.style.display = "none";
+    divform.style.display = "none";
   }
   else{
     return;
   }
 });
 
-passView.addEventListener('click', () => {
+/* passView.addEventListener('pointerdown', () => {
   if(passInput.getAttribute('type') == 'password'){
     passInput.setAttribute('type', 'text');
   }
   else{
     passInput.setAttribute('type', 'password');
   }
+}); */
+
+//show/hide password
+passView.addEventListener('pointerdown', () => {
+  passInput.setAttribute('type', 'text');
+});
+
+passView.addEventListener('pointerup', () => {
+    passInput.setAttribute('type', 'password');
+});
+
+//check form validation
+form.addEventListener('blur', function (event) {
+  const isValid = event.target.validity.valid;
+
+  if(event.target == emailInput && !isValid)
+    emailInput.setCustomValidity('Please enter valid email. example@mail.com');
+  else if(event.target == passInput && !isValid)
+    passInput.setCustomValidity('Please use at least 6 characters.');
+
+  const message = event.target.validationMessage;
+  const connectedValidationId = event.target.getAttribute('aria-describedby');
+  const connectedValidation = connectedValidationId ? document.getElementById(connectedValidationId) : false;
+
+  if (connectedValidation && message && !isValid) { 
+    connectedValidation.innerText = message;
+  } else {
+    connectedValidation.innerText = '';
+  }
+  passInput.setCustomValidity('');
+  emailInput.setCustomValidity('');
+}, true);
+
+//send form
+form.addEventListener("submit", (event) => {
+
+  event.preventDefault();
+
+  var data = new FormData(form);
+
+  console.log(data);
+
+  popup.style.display = "none";
+  divform.style.display = "none";
 });
